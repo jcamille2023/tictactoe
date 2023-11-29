12c94fd1dba71fbd240786a6ccf3a32f33a010a5
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-11-28 19:23:23 -0500

    Update create.js

--- play/create.js
+++ play/create.js
@@ -1,3 +1,4 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 import { getDatabase, set, ref, onValue, get,child,remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
@@ -7,10 +8,10 @@ var player_1 = "";
 var player_2 = "";
 var turn = "";
 var opponentId = "";
-var n = 0;
 var positions = {};
 var gameId = 0;
 var game_data = {};
+var game_start = false;
 var win_combos =  [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 var positions_used = [];
 
@@ -29,6 +30,13 @@ const auth = getAuth(app);
 const database = getDatabase();
 const dbRef = ref(getDatabase());
 
+function delete_session() {
+	let game_ref = ref(database, "/games/" + gameId);
+	remove(game_ref);
+	console.log("Game session deleted.");
+}
+window.delete_session = delete_session;
+
 function random_number_gen() { // generates gameId
 	return Math.floor(Math.random() * 9999);
 }
@@ -45,6 +53,7 @@ function declare_win() {
 		
 	});
 }
+
 
 function check_win() {
 	positions_used = [];
@@ -80,6 +89,7 @@ function check_win() {
     return false;
 }
 
+
 function deactivate_buttons() {
 	for (let n = 1; n != 10; n++) { 
         	let b = n.toString();
@@ -103,167 +113,65 @@ if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/auth.user
     playerId = user.uid;
+    player_1 = playerId
     console.log(playerId);
     console.log("User is signed in");
-	  var game_state = "new";
-	  gameId = random_number_gen();
+    document.getElementById("user_id").innerHTML += playerId;
+    gameId = random_number_gen()
     document.getElementById("game_id").innerHTML = gameId;
-	  set(ref(database, "/games/" + gameId), {
-		  player_1: playerId,
-	  });
-	  player_1 = playerId;
-	  turn = playerId;
-	  document.getElementById("user_turn").innerHTML += "(" + playerId + ")";
-	  const gamesRef = ref(database, 'games/' + gameId);
-	  onValue(gamesRef, (snapshot) => {
-		  n += 1;
-		  console.log(n);
-		  var data = snapshot.val();
-		  player_2 = data['player_2'];
-		  console.log(player_2);
-		  opponentId = player_2;
-		  if (n == 2) {
-			  document.getElementById("opponent_id").innerHTML += opponentId;
-			  activate_buttons();
-		  }
-		  else {
-        console.log("opponent exists or too early");
-        if(data.turn != null) {
-	        var player_turn = data.turn;
-	        console.log(player_turn);
-	        if (player_turn == playerId) {
-		        activate_buttons();
-		        document.getElementById("user_turn").innerHTML = "X's turn (" + playerId + ")";
-		      }
-		      else {
-            deactivate_buttons()
-		        document.getElementById("user_turn").innerHTML = "O's turn (" + opponentId + ")";
-		      } 
-        }
-      }
-	});
+    set(ref(database, "/games/" + gameId + "/players"), {
+		player_1: playerId,
+    });
+    const playersRef = ref(database, 'games/' + gameId + '/players')
+    onValue(playersRef, (snapshot) => {
+	    const data = snapshot.val();
+	    if(data.player_2 != null) {
+	    	player_2 = data.player_2;
+	    	opponentId = player_2;
+	    	activate_buttons()
+		
+	    }
+	    
+    });
+      
+   
 
-
-  
 }
-
 else {
 	console.log("User is signed out");
 }
 });
-const positionsRef = ref(database, 'games/' + gameId + '/positions');
-onValue(positionsRef, (snapshot) => {
-	var data = snapshot.val();
-	console.log(data);
-	if(data == null) {
-		console.log("no position data");
-	}
-	else if(check_win() == false) {
-		for(let n = 1; n < 10; n++) {
-    			let button_id = "button_" + n.toString();
-			console.log(button_id + " changed");
-    			if (data[n] == player_1) {
-      				document.getElementById(button_id).innerHTML = "X";
-    			}
-    			else if (data[n] == player_2) {
-      				document.getElementById(button_id).innerHTML = "O";
-    			}
-  		}
-	}
-	else {
- 		declare_win();
-		for(let n = 1; n < 10; n++) {
-    			let button_id = "button_" + n.toString();
-			console.log(button_id + " changed");
-    			if (data[n] == player_1) {
-      				document.getElementById(button_id).innerHTML = "X";
-    			}
-    			else if (data[n] == player_2) {
-      				document.getElementById(button_id).innerHTML = "O";
-    			}
-  		}
-		
-   	}
-	
-});
 
-const winRef = ref(database, 'games/' + gameId + '/win');
-onValue(winRef, (snapshot) => {
-	const data = snapshot.val();
-	if(data != null) {
-	console.log(data);
-	deactivate_buttons();
-	document.getElementById("user_turn").remove();
-	if(data.winner == playerId) {
-		document.getElementById("game_winner").innerHTML = "O wins (" + playerId + ")";
-	}
-	else {
-		document.getElementById("game_winner").innerHTML = "X wins (" + opponentId + ")";
-	}
-	}
-	
-});
+		
+
 
 
 function move_multi_2(user_id,button_number) {
 	get(child(dbRef, '/games/' + gameId + "/positions")).then((snapshot) => {
 			positions = snapshot.val();
 			console.log(positions);
-			if (positions != null) {
-				positions[button_number] = user_id;
-				console.log(positions);
-				set(ref(database,"/games/" + gameId + "/positions"), positions);
-			}
-			else {
-				positions = {};
-				positions[button_number] = user_id;
-				console.log(positions);
-				set(ref(database,"/games/" + gameId + "/positions"), positions);
-			}
+			positions[button_number] = user_id;
+			console.log(positions);  
+			set(ref(database,"/games/" + gameId + "/positions"), positions);
 			
 		});
 }
 function move_multi(button_number) {
-	console.log(player_2);
-	if (playerId == player_1) {
-		 // button_id = "button" + button_number;
+	console.log(opponentId);
+	// button_id = "button" + button_number;
 		// document.getElementById(button_id).innerHTML = "X"; when change is detected database will change the button, not the computer
 		get(child(dbRef, '/games/' + gameId)).then((snapshot) => {
 			var data = snapshot.val();
 			game_data = data;
 			console.log(game_data);
-			console.log(player_2);
-			game_data.turn = player_2;
+			console.log(opponentId);
+			game_data.turn = opponentId;
 			console.log(game_data);
 			set(ref(database,"/games/" + gameId), game_data);
 		});
-			move_multi_2(player_1,button_number); // code sections farther down have been running out of order, therefore, calling them in a separate function will prevent this.
-	}
-	else {
-		 // button_id = "button" + button_number;
-		// document.getElementById(button_id).innerHTML = "X"; when change is detected database will change the button, not the computer
-		get(child(dbRef, '/games/' + gameId)).then((snapshot) => {
-			var data = snapshot.val();
-			game_data = data;
-			console.log(game_data);
-			console.log(player_1);
-			game_data.turn = player_1;
-			console.log(game_data);
-			set(ref(database,"/games/" + gameId), game_data);
 			move_multi_2(player_2,button_number); // code sections farther down have been running out of order, therefore, calling them in a separate function will prevent this.
-			
-			
-		});
 	}
-}
 window.move_multi = move_multi;
 
 
-
-
- 
-
-
-
-	
 
